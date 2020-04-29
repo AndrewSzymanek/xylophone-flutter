efa32f5e6f9656d62dd635a82c8be995fdb4920a
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2020-04-29 09:52:59 -0500

    created playsound and buildkey methods, adjusted UI so buttons stretched vertically and horizontally

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int noteNum) {
+    final player = AudioCache();
+    player.play('note$noteNum.wav');
+  }
+
+  Expanded buildKey(int noteNum, Color color) =>
+    Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: (){
+          playSound(noteNum);
+        },
+      ),
+    );
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(1, Colors.red),
+                buildKey(2, Colors.orange),
+                buildKey(3, Colors.yellow),
+                buildKey(4, Colors.green),
+                buildKey(5, Colors.lightBlue),
+                buildKey(6, Colors.indigo),
+                buildKey(7, Colors.purple),
+              ],
+            ),
         ),
       ),
     );
